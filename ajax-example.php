3c130a16d66d52761d3d8f3e286d62cdd0c51cd7
jd90 <1418146@rgu.ac.uk>
2016-02-28 15:30:46 +0000

    k

--- ajax-example.php
+++ ajax-example.php
@@ -9,7 +9,23 @@
 $email = $_GET['email'];
 $password = $_GET['password'];
 
-echo "Query: ".$email." ".$password;
+//echo "Query: ".$email." ".$password;
+
+
+$conn = new PDO ( "sqlsrv:server = tcp:bbsqldb.database.windows.net,1433; Database = SQL_BB", "teamdsqldb", "Sql20022016*");
+$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+try{
+  $st = $conn-> query("SELECT * FROM [Owner] WHERE [email] = '".$email."'");
+
+foreach($st->fetchAll() as $row) {
+
+        echo "emails matching your search ".$row[email];
+
+}
+
+}
+catch(PDOException $e)
+{echo "$e";}
 
 
 ?>
