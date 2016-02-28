ace43274911b5b840b6f0df477dc568bee3eafaa
jd90 <1418146@rgu.ac.uk>
2016-02-28 14:48:27 +0000

    k

--- verifyUserFile.php
+++ verifyUserFile.php
@@ -5,43 +5,26 @@
 <?php
 
 
-$email= intval($_GET['emails']);
-
-if($email == ""){
-    echo "<p>error</p>";//
-}
+$email= intval($_GET['email']);
 
 $conn = new PDO ( "sqlsrv:server = tcp:bbsqldb.database.windows.net,1433; Database = SQL_BB", "teamdsqldb", "Sql20022016*");
 $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-try {
-    $st = $conn->query("SELECT * FROM [Owner] WHERE [email] = '" . $email . "'");
+try{
+    $st = $conn-> query("SELECT * FROM [Owner] WHERE [email] = '".$email."'");
 
-    if ($num_rows = mysql_num_rows($st) > 0) {
+    foreach($st->fetchAll() as $row) {
 
-        $newhtml =
-            <<<NEWHTML
-
-           <p>as there are ".$num_rows." - you are already a user?</p>
-
-NEWHTML;
-
-
-    } else {
-        $newhtml =
-            <<<NEWHTML
-
-           <p>as there are ".$num_rows." - aye, keep gawn</p>
-
-NEWHTML;
-
-
-        //foreach($st->fetchAll() as $row) {
-        print($newhtml);
+        echo "<p>emails matching your search ".$row[email]. "</p>";
 
     }
+
+    //foreach($st->fetchAll() as $row) {
+
+
 }
 catch(PDOException $e)
 {print"$e";}
+
 
 ?>
 </body>
