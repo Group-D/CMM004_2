79aa7bb6cfc579ff6075406b1d42a234253f2c79
jd90 <1418146@rgu.ac.uk>
2016-02-29 11:08:59 +0000

    k

--- script.js
+++ script.js
@@ -23,14 +23,14 @@ $(document).ready(function(){
                     $("#result").innerHtml = result;
                     }
                     else{
-                        alert("That's you signed in, chief!");
+                        alert("That's you signed in, chief!£");
 
                         //var form = $('<form action="http://bbhubapp.azurewebsites.net/Home.php" method="POST">' +
                           //  '<input type="text" name="user" value="'+email+'" />' +
                             //'</form>');
                         //$('body').append(form);
 
-                        var form =$("#myForm").submit;
+                        var form =$("#myForm").submit();
                         //form.submit();
                         //$.session.set("user", ""+email);
                         //window.location.href = "http://bbhubapp.azurewebsites.net/Home.php";
